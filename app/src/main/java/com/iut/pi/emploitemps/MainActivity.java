a5f3bc9d8357a4dc8b900638b1b66eb8304a2e34
Affolaby Paul-Ivan <paulivanaffolabyn@hotmail.com>
2016-03-23 15:54:33 +0100

    Calendar beginning

--- app/src/main/java/com/iut/pi/emploitemps/MainActivity.java
+++ app/src/main/java/com/iut/pi/emploitemps/MainActivity.java
@@ -1,21 +1,18 @@
 package com.iut.pi.emploitemps;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private Spinner spin;
     private Button button;
     private String groupe;
@@ -65,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void doOk(View view){
-        Intent intent = new Intent(getApplicationContext(),ResultActivity.class);
+        Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
         Bundle bundle = new Bundle();
         groupe = spin.getSelectedItem().toString().trim();
         bundle.putString("Groupe", groupe);
--- app/src/main/java/com/iut/pi/emploitemps/ResultActivity.java
+++ app/src/main/java/com/iut/pi/emploitemps/ResultActivity.java
@@ -1,14 +1,10 @@
 package com.iut.pi.emploitemps;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.Toast;
 
-public class ResultActivity extends AppCompatActivity {
+public class ResultActivity extends Activity {
     private Bundle extras;
 
     @Override
