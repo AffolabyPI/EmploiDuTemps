2593dfc6248b4946007963678f7a9add3a2f7ea5
Benjamin Ducauroy <benjamin.ducauroy@etudiant.univ-lille1.fr>
2016-03-24 14:17:29 +0100

    Logo activity

---
+++ app/src/main/java/com/iut/pi/emploitemps/LogoActivity.java
@@ -0,0 +1,37 @@
+package com.iut.pi.emploitemps;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class LogoActivity extends Activity {
+    private Intent intent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logo);
+        intent = new Intent(getApplicationContext(), MainActivity.class);
+        try{
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(intent);
+                }
+            }, 3 * 1000);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+}
